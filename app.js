8d65caa70ea0c35b978148e20d827837a273bb8a
gloatingorc <gloatingorc@gmail.com>
2023-04-20 16:49:19 +0200

    fix bug et connection to local database

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express");
+require('dotenv').config()
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.json()); 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(express.json());
+
+const dbUrl = process.env.DB_URL
+mongoose.set('strictQuery', false)
+mongoose.connect(dbUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+const db = mongoose.connection
+db.on('error', (error)=>console.log(error));
+db.once('open', (open)=>console.log('connected to database'));
 
 app.post('/login', (req, res)=>{
-       console.log(req.body)
-        res.json({message: "success"});
+        console.log(req.body)
+        res.json({message: req.body});
 })
 
 app.listen(3000, ()=>{
